d3a20ea7e21c8eccc3c54df3caebb2868698971b
Zeide <git.lagging775@passmail.net>
2026-06-16 23:52:41 +1200

    feat: update greeter

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
--- nix/greeter.nix
+++ nix/greeter.nix
@@ -2,114 +2,82 @@ self: {
   config,
   pkgs,
   lib,
-  inputs,
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) system;
 
   cfg = config.programs.shiny-shell-greeter;
 
-  hasHomeManager = config ? home-manager;
-  userExists = hasHomeManager && (config.home-manager.users ? "${cfg.user}");
+  toLua = lib.generators.toLua {};
 
-  userShinyShellSettings =
-    if userExists
-    then config.home-manager.users.${cfg.user}.programs.shiny-shell.settings
-    else {};
+  renderLuaArgs = value:
+    if lib.isAttrs value && value ? _args
+    then lib.concatMapStringsSep ", " toLua value._args
+    else toLua value;
 
-  userHyprlandSettings =
-    if userExists
-    then config.home-manager.users.${cfg.user}.wayland.windowManager.hyprland.settings
-    else {};
+  renderLuaCalls = name: values:
+    lib.concatMapStringsSep "\n" (value: "hl.${name}(${renderLuaArgs value})") (
+      if lib.isList values
+      then values
+      else [values]
+    );
 
-  shinyShellConf = pkgs.writeText "shiny-shell-greeter.json" (builtins.toJSON userShinyShellSettings);
+  shinyShellConf = pkgs.writeText "shiny-shell-greeter.json" (builtins.toJSON cfg.settings);
 
-  baseHyprConfig = ''
-    general {
-      gaps_in = 0
-      gaps_out = 0
-      border_size = 0
-    }
+  hyprConf = pkgs.writeText "shiny-shell-greeter-hyprland.lua" ''
+    hl.config(${toLua {
+      general = {
+        gaps_in = 0;
+        gaps_out = 0;
+        border_size = 0;
+      };
 
-    decoration {
-      rounding = 0
+      decoration = {
+        rounding = 0;
 
-      blur {
-        enabled = false
-      }
+        blur.enabled = false;
+        shadow.enabled = false;
+      };
 
-      shadow {
-        enabled = false
-      }
-    }
+      animations.enabled = false;
 
-    animations {
-      enabled = false
-    }
+      misc = {
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        disable_autoreload = true;
+        background_color = "rgb(000000)";
+      };
 
-    misc {
-      disable_hyprland_logo = true
-      disable_splash_rendering = true
-      disable_autoreload = true
-      background_color = rgb(000000)
-    }
+      xwayland.enabled = false;
 
-    xwayland {
-      enabled = false
-    }
+      cursor = {
+        invisible = true;
+        no_hardware_cursors = 0;
+      };
 
-    cursor {
-      invisible = true
-      no_hardware_cursors = 0
-    }
+      ecosystem = {
+        no_update_news = true;
+        no_donation_nag = true;
+      };
 
-    ecosystem {
-      no_update_news = true
-      no_donation_nag = true
-    }
+      input = cfg.hyprlandSettings.input;
+    }})
 
-    env = SHINYSHELL_CONFIG,${shinyShellConf}
-    env = SHINYSHELL_GREETER_SESSION,${cfg.session}
-    env = SHINYSHELL_GREETER_USER,${cfg.user}
+    ${renderLuaCalls "monitor" cfg.hyprlandSettings.monitor}
+    ${renderLuaCalls "device" cfg.hyprlandSettings.device}
 
-    exec-once = ${cfg.package}/bin/shiny-shell-greeter; ${cfg.hyprlandPackage}/bin/hyprctl dispatch exit
+    hl.env("SHINYSHELL_CONFIG", ${toLua shinyShellConf})
+    hl.env("SHINYSHELL_GREETER_SESSION", ${toLua cfg.session})
+    hl.env("SHINYSHELL_GREETER_USER", ${toLua cfg.user})
+
+    hl.on("hyprland.start", function()
+      hl.exec_cmd(${toLua "${cfg.package}/bin/shiny-shell-greeter; ${cfg.hyprlandPackage}/bin/hyprctl dispatch exit"})
+    end)
   '';
 
-  monitorHyprConfig = ''
-    monitor = , preferred, auto, auto
+  greeterSession = pkgs.writeShellScript "shiny-shell-greeter-session" ''
+    exec ${cfg.hyprlandPackage}/bin/start-hyprland -- --config ${hyprConf} >/dev/null 2>&1
   '';
-
-  userHyprConfig = {
-    monitor =
-      if userHyprlandSettings ? monitor
-      then userHyprlandSettings.monitor
-      else [", preferred, auto, auto"];
-
-    input =
-      if userHyprlandSettings ? input
-      then userHyprlandSettings.input
-      else {};
-
-    device =
-      if userHyprlandSettings ? device
-      then userHyprlandSettings.device
-      else [];
-  };
-
-  hyprConf = pkgs.writeText "shiny-shell-greeter-hyprland.conf" (
-    if cfg.useHyprlandUserOptions
-    then ''
-      ${inputs.home-manager.lib.hm.generators.toHyprconf {
-        attrs = userHyprConfig;
-        importantPrefixes = config.home-manager.users.${cfg.user}.wayland.windowManager.hyprland.importantPrefixes;
-      }}
-      ${baseHyprConfig}
-    ''
-    else ''
-      ${monitorHyprConfig}
-      ${baseHyprConfig}
-    ''
-  );
 in {
   options = with lib; {
     programs.shiny-shell-greeter = {
@@ -137,28 +105,51 @@ in {
         description = "Session to open when the greeter is run";
       };
 
-      useShinyShellUserOptions = mkEnableOption "the usage of Shiny Shell user options (home-manager is required)";
-      useHyprlandUserOptions = mkEnableOption "the usage of Hyprland user options (home-manager is required)";
+      settings = mkOption {
+        type = types.attrs;
+        default = {};
+        description = "Shiny Shell settings to use for the greeter";
+      };
+
+      hyprlandSettings = mkOption {
+        type = types.submodule {
+          options = {
+            monitor = mkOption {
+              type = with types; either attrs (listOf attrs);
+              default = {
+                output = "";
+                mode = "preferred";
+                position = "auto";
+                scale = "auto";
+              };
+              description = "Hyprland monitor settings to use for the greeter";
+            };
+
+            input = mkOption {
+              type = types.attrs;
+              default = {};
+              description = "Hyprland input settings to use for the greeter";
+            };
+
+            device = mkOption {
+              type = with types; either attrs (listOf attrs);
+              default = [];
+              description = "Hyprland device settings to use for the greeter";
+            };
+          };
+        };
+        default = {};
+        description = "Hyprland settings to use for the greeter";
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = cfg.useShinyShellUserOptions -> userExists;
-        message = "programs.shiny-shell-greeter: useShinyShellUserOptions is enabled but home-manager is not found or user '${cfg.user}' is not defined.";
-      }
-      {
-        assertion = cfg.useHyprlandUserOptions -> userExists;
-        message = "programs.shiny-shell-greeter: useHyprlandUserOptions is enabled but home-manager is not found or user '${cfg.user}' is not defined.";
-      }
-    ];
-
     services.greetd = {
       enable = true;
       settings = {
         default_session = {
-          command = "${cfg.hyprlandPackage}/bin/start-hyprland -- --config ${hyprConf}";
+          command = "${greeterSession}";
           user = "greeter";
         };
       };
